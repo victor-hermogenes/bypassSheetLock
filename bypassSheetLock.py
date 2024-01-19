83b33973c25bf5bc9e078223b89d1a54033975ff
victor-hermogenes <victorltrgh@gmail.com>
2024-01-19 17:00:10 -0300

    Trying to create progress bar and progress following to the program

--- bypassSheetLock.py
+++ bypassSheetLock.py
@@ -2,8 +2,24 @@ import PySimpleGUI as sg
 from removefunctions import remove_sheet_protection as rsp
 from removefunctions import remove_workbook_protection as rwp
 
+
+def upb(progress_bar, output_box, progress, message):
+    """Update the progress bar and output box."""
+    progress_bar.UpdateBar(progress)
+    output_box.Update(f'{message}\n', append=True)
+
+
+def layout_progress():
+    layout_progress = [
+        [sg.Text("Progress")],
+        [sg.ProgressBar(100, orientation='h', size=(20, 20), key='progressbar')],
+        [sg.Multiline(default_text='Events will appear here...', size=(40, 10), key='output')],
+    ]
+    
+    return layout_progress
+
 # Create interface
-layout = [
+main_layout = [
     [sg.Text("Please, this program were created to unlock sheets that you forgot!")],
     [sg.Text("Any other ilegal uses will be on user's own responsability.")],
     [sg.Text("File Path: "), sg.Input(), sg.FileBrowse()],
@@ -11,22 +27,38 @@ layout = [
     [sg.Text("Created by Victor G. Hermogenes")],
 ]
 
+
+
 # Create window that will open to show interface
-window = sg.Window("bypassSheetLock", layout)
+main_window = sg.Window("bypassSheetLock", main_layout)
 
 # Creating events for buttons
-while True:
-    event, values = window.read()
-    # Closes the window
-    if event == sg.WIN_CLOSED or event == 'Close':
-        break
-    # Run unprotect functions
-    elif event == "Unprotect":
-        file_path = values[0]
-        rwp(file_path)
-        rsp(file_path)
-    else:
-        break
+try:
+    while True:
+        event, values = main_window.read()
+        # Closes the window
+        if event == sg.WIN_CLOSED or event == 'Close':
+            break
+        # Run unprotect functions
+        elif event == "Unprotect":
+            file_path = values[0]
 
-window.close()
+            # Define a lambda function for the callback
+            def callback(message, progress=0):
+                upb(progress_bar, output_box, progress, message)
+                popup_window.refresh()  # Refresh the window to update the GUI
 
+            # Open popup window
+            popup_window = sg.Window("Unprotection...", layout_progress()).finalize()
+            progress_bar = popup_window['progressbar']
+            output_box = popup_window['output']
+
+            # Call functions with callback
+            rwp(file_path, callback)
+            rsp(file_path, callback)
+        else:
+            break
+
+    main_window.close()
+except Exception as e:
+    print(f"Error: {e}")
--- removefunctions.py
+++ removefunctions.py
@@ -1,59 +1,101 @@
 import zipfile
 import re
+import os
+import time
 from tempfile import mkdtemp
 from shutil import rmtree, copyfile
-import os
 
-def remove_workbook_protection(file_path):
+total_steps = 30
+step_count = 0
+
+def remove_workbook_protection(file_path, callback):
+    global step_count
     try:
+        step_count += 1
+        callback(100 * (step_count / total_steps),"Creating temporary directory...")
         temp_dir = mkdtemp()
+        step_count += 1
+        callback(100 * (step_count / total_steps),f"Directory {temp_dir} created!")
 
         # Create a backup of the original file
+        step_count += 1
+        callback(100 * (step_count / total_steps),"Creating backup for safety...")
         backup_file_path = file_path + '.backup'
         copyfile(file_path, backup_file_path)
-        print(f"Backup created at: {backup_file_path}")
+        step_count += 1
+        callback(100 * (step_count / total_steps),f"Backup created at: {backup_file_path}!")
 
         # Open the Excel file as a zip file and extract its contents
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
             zip_ref.extractall(temp_dir)
+        step_count += 1
+        callback(100 * (step_count / total_steps),f"Extracting file at {temp_dir}...")
 
         workbook_path = os.path.join(temp_dir, "xl", "workbook.xml")
         
         # Read the workbook.xml file
         with open(workbook_path, 'r', encoding='utf-8') as file:
             xml_content = file.read()
+        step_count += 1
+        callback(100 * (step_count / total_steps),f"Reading xml content: {xml_content}...")
+        step_count += 1
+        callback(100 * (step_count / total_steps),f"Looking for workbook protection...")
 
         # Use regex to find and remove the workbookProtection element
+        step_count += 1
+        callback(100 * (step_count / total_steps),"Workbook protection found!")
+        step_count += 1
+        callback(100 * (step_count / total_steps),"Removing workbook protection...")
         xml_content = re.sub(r'<workbookProtection[^>]*>', '', xml_content)
+        step_count += 1
+        callback(100 * (step_count / total_steps),"Workbook protection removed!")
+
 
         # Write the modified content back to workbook.xml
         with open(workbook_path, 'w', encoding='utf-8') as file:
+            step_count += 1
+            callback(100 * (step_count / total_steps),"Saving modifications...")
             file.write(xml_content)
+            step_count += 1
+            callback(100 * (step_count / total_steps),"Modifications saved!")
 
         # Re-zip the extracted files back into the .xlsx file
         with zipfile.ZipFile(file_path, 'w', zipfile.ZIP_DEFLATED) as zip_out:
+            step_count += 1
+            callback(100 * (step_count / total_steps),"Zipping file again...")
             for root, _, files in os.walk(temp_dir):
                 for file in files:
                     full_path = os.path.join(root, file)
                     arcname = os.path.relpath(full_path, temp_dir)
                     zip_out.write(full_path, arcname)
+            step_count += 1
+            callback(100 * (step_count / total_steps),"File zipped!")
 
         rmtree(temp_dir)
-        print("Workbook protection successfully removed.")
+        step_count += 1
+        callback(100 * (step_count / total_steps),"Workbook protection successfully removed.")
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        callback(f"An error occurred: {e}")
 
 
-def remove_sheet_protection(file_path):
+def remove_sheet_protection(file_path, callback):
+    global step_count
     try:
+        step_count += 1
+        callback(100 * (step_count / total_steps),"Creating temporary directory...")
         temp_dir = mkdtemp()
+        step_count += 1
+        callback(100 * (step_count / total_steps),f"Directory {temp_dir} created!")
 
         # Open the Excel file as a zip file and extract its contents
         with zipfile.ZipFile(file_path, 'r') as zip_ref:
             zip_ref.extractall(temp_dir)
+            step_count += 1
+            callback(100 * (step_count / total_steps),f"Extracting file at {temp_dir}...")
 
         sheet_dir = os.path.join(temp_dir, "xl", "worksheets")
+        
         for sheet_file in os.listdir(sheet_dir):
             if sheet_file.endswith('.xml'):
                 sheet_path = os.path.join(sheet_dir, sheet_file)
@@ -61,24 +103,43 @@ def remove_sheet_protection(file_path):
                 # Read the sheet XML file
                 with open(sheet_path, 'r', encoding='utf-8') as file:
                     xml_content = file.read()
-
+                step_count += 1
+                callback(100 * (step_count / total_steps),f"Reading xml content: {xml_content}...")
+                step_count += 1
+                callback(100 * (step_count / total_steps),f"Looking for sheet protection...")
+                
                 # Use regex to find and remove the sheetProtection element
+                step_count += 1
+                callback(100 * (step_count / total_steps),"Sheet protection found!")
+                step_count += 1
+                callback(100 * (step_count / total_steps),"Removing sheet protection...")
                 xml_content = re.sub(r'<sheetProtection[^>]*>', '', xml_content)
+                step_count += 1
+                callback(100 * (step_count / total_steps),"Sheet protection removed!")
 
                 # Write the modified content back to the sheet XML file
                 with open(sheet_path, 'w', encoding='utf-8') as file:
+                    step_count += 1
+                    callback(100 * (step_count / total_steps),"Saving modifications...")
                     file.write(xml_content)
+                    step_count += 1
+                    callback(100 * (step_count / total_steps),"Modifications saved!")
 
         # Re-zip the extracted files back into the .xlsx file
         with zipfile.ZipFile(file_path, 'w', zipfile.ZIP_DEFLATED) as zip_out:
+            step_count += 1
+            callback(100 * (step_count / total_steps),"Zipping file again...")
             for root, _, files in os.walk(temp_dir):
                 for file in files:
                     full_path = os.path.join(root, file)
                     arcname = os.path.relpath(full_path, temp_dir)
                     zip_out.write(full_path, arcname)
+            step_count += 1
+            callback(100 * (step_count / total_steps),"File zipped!")
 
         rmtree(temp_dir)
-        print("Sheet protection successfully removed from all worksheets.")
+        step_count += 1
+        callback(100 * (step_count / total_steps),"Sheet protection successfully removed from all worksheets.")
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        callback(f"An error occurred: {e}")
